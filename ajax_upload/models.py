2352a3b0885cf99da19fb01d4b98954a9cef7066
Bernhard Vallant <bernhard@vallant.org>
2024-07-24 11:26:52 +0200

    Django 4 compatibility

--- ajax_upload/models.py
+++ ajax_upload/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from ajax_upload.settings import FILE_FIELD_MAX_LENGTH, UPLOAD_TO_DIRECTORY
 
